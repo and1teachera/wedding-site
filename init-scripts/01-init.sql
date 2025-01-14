1da510303fbb8605d9d708022f601537f01e925a
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-01-14 12:27:17 +0200

    build: Address warnings in CI/CD workflow

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -2,7 +2,7 @@
 USE wedding_site;
 
 CREATE USER 'monitoring'@'localhost' IDENTIFIED BY '${MONITORING_PASSWORD}';
-GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'monitoring'@'localhost';
+GRANT SELECT, SHOW DATABASES, PROCESS, REPLICATION CLIENT ON *.* TO 'monitoring'@'localhost';
 
 GRANT ALL PRIVILEGES ON wedding_site.* TO 'wedding_user'@'%';
 

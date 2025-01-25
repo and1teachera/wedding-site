d82b251e1a2ba0361650643304aa980d8c793629
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-01-25 18:04:36 +0200

    feat: Create a landing page

---
+++ frontend/src/app/landing/components/navigation/navigation.component.ts
@@ -0,0 +1,23 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css'],
+})
+export class NavigationComponent {
+  isMenuOpen = false;
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}

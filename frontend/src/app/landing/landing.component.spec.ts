2057aef6e569f65d4372a37fa2d7e178e2250b2b
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-01-28 18:28:53 +0200

    fix: Fix landing component spec test

--- frontend/src/app/landing/landing.component.spec.ts
+++ frontend/src/app/landing/landing.component.spec.ts
@@ -1,24 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
-import {AppComponent} from "../app.component";
 
-describe('AppComponent', () => {
+describe('LandingComponent', () => {
+  let component: LandingComponent;
+  let fixture: ComponentFixture<LandingComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, LandingComponent],
-    }).compileComponents();
+      imports: [LandingComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LandingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'frontend' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });

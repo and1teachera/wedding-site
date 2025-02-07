20b4ea5330d2c12eb190c0f24df0fca67c8c58e5
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-02-07 21:28:57 +0200

    fix: Fix the integration tests

--- backend/src/test/java/com/zlatenov/wedding_backend/WeddingBackendApplicationTests.java
+++ backend/src/test/java/com/zlatenov/wedding_backend/WeddingBackendApplicationTests.java
@@ -2,12 +2,12 @@ package com.zlatenov.wedding_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class WeddingBackendApplicationTests {
-
+@ActiveProfiles("test")
+public class WeddingBackendApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }

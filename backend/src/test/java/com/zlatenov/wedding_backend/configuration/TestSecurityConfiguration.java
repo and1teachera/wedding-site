cc2856b1a949369b8787ad8569987f01ab882d21
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-02-13 21:34:16 +0200

    test: Create tests for AuthenticationController

---
+++ backend/src/test/java/com/zlatenov/wedding_backend/configuration/TestSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.zlatenov.wedding_backend.configuration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+@EnableWebSecurity
+public class TestSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+}

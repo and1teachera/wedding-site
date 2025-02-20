e97a7beb21cf6364f927ca8c4f3a12117109bd67
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-02-20 23:38:22 +0200

    feat: Implement user confirm rsvp

--- frontend/src/app/rsvp/services/rsvp.service.spec.ts
+++ frontend/src/app/rsvp/services/rsvp.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RsvpService } from './rsvp.service';
 
 describe('RsvpService', () => {
   let service: RsvpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RsvpService]
+    });
     service = TestBed.inject(RsvpService);
   });
 

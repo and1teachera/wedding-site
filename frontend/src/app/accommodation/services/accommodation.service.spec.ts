56b9ecbe19410aa20a3e72efa8610a66a1793845
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-02-21 22:46:42 +0200

    feat: Provide the users the possibility to book rooms

--- frontend/src/app/accommodation/services/accommodation.service.spec.ts
+++ frontend/src/app/accommodation/services/accommodation.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AccommodationService } from './accommodation.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RsvpService} from "../../rsvp/services/rsvp.service";
 
 describe('AccommodationService', () => {
   let service: AccommodationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AccommodationService]
+    });
     service = TestBed.inject(AccommodationService);
   });
 

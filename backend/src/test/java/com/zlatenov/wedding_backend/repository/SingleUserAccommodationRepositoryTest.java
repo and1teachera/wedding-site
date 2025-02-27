fce7fbbd7bd3188e779c840d28659a974686a51f
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-02-27 10:53:54 +0200

    test: Create missing repository tests

---
+++ backend/src/test/java/com/zlatenov/wedding_backend/repository/SingleUserAccommodationRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.zlatenov.wedding_backend.repository;
+
+/**
+ * @author Angel Zlatenov
+ */
+
+public class SingleUserAccommodationRepositoryTest {
+}
---
+++ backend/src/test/java/com/zlatenov/wedding_backend/repository/UserResponseRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.zlatenov.wedding_backend.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Angel Zlatenov
+ */
+class UserResponseRepositoryTest {
+
+}

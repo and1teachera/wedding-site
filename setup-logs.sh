4b5f47c9da45d8342ffc7633e57c2590a9af848e
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-03-03 22:52:53 +0200

    feat: introduce loki

--- setup-logs.sh
+++ setup-logs.sh
@@ -3,7 +3,6 @@
 # Create log directories
 mkdir -p ./logs/nginx
 
-# In a production environment, you would set appropriate permissions
-# chmod -R 777 ./logs
+chmod -R 777 ./logs
 
 echo "Log directories created successfully"

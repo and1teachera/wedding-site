710927de1dee32ab89947b56f20db08a55b1eca7
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-03-08 22:18:03 +0200

    feat: Integrate prometheus for app monitoring

---
+++ backend/src/main/java/com/zlatenov/wedding_backend/configuration/MetricsConfig.java
@@ -0,0 +1,45 @@
+package com.zlatenov.wedding_backend.configuration;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.tomcat.TomcatMetrics;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for custom Prometheus metrics
+ */
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config()
+                .commonTags("application", "wedding-backend");
+    }
+
+    @Bean
+    TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+
+    @Bean
+    JvmMemoryMetrics jvmMemoryMetrics() {
+        return new JvmMemoryMetrics();
+    }
+
+    @Bean
+    JvmThreadMetrics jvmThreadMetrics() {
+        return new JvmThreadMetrics();
+    }
+
+    @Bean
+    ProcessorMetrics processorMetrics() {
+        return new ProcessorMetrics();
+    }

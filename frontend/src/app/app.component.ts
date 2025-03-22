21c6ca828d84b25ad82db5232e8f2d53728a1884
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-03-22 11:33:01 +0200

    feat: Add notifications in the rsvp flow

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {TokenService} from "./auth/services/token.service";
 import {CommonModule} from "@angular/common";
 import {NavigationComponent} from "./landing/components/navigation/navigation.component";
+import {NotificationComponent} from "./shared/components/notification/notification.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [CommonModule, RouterOutlet, NavigationComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
